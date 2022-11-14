108e60c15f1d894de92a22a37cbea5cff33de0f8
roman0f <92151944+roman0f@users.noreply.github.com>
2022-11-14 12:27:33 +0100

    #32pwhpa
    aggiunta la funzione per il calcolo della distanza euclidea

--- index.php
+++ index.php
@@ -10,8 +10,21 @@ $spatt=0;
 $spdefense=0;
 $speed=0;
 
-
+//presi i valori dal csv 
 $csv = array_map('str_getcsv', file('data/pokemon.csv'));
-print_r($csv);
+//print_r($csv);
+
+function distanzaEuclidea($num1,$num2,$length){
+    $distance=0;
+    for($i=0;$i<$length;$i++){
+        $distance += pow(($num1 - $num2), 2);
+
+    }
+       
+    return sqrt($distance);
+
+}
+
+echo distanzaEuclidea(10,5,1);
 //(1;1) (721;13)
 ?>
